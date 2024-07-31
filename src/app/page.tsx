84b301fa5a9ec419283e6a6c88e4fea2a2da6b6c
MGSimard <g.marcgs@gmail.com>
2024-07-30 21:45:17 -0400

    Testing db on deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
 import { Searchbar } from "@/components/searchbar/Searchbar";
+import { db } from "@/server/db";
 
-export default function Page() {
+export default async function Page() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="index-main">
       <Searchbar />
       <div>
         <h2>THIS APP IS IN ACTIVE DEVELOPMENT MODE.</h2>
+      </div>
+      <div>
+        Database test:{" "}
+        {posts.map((post) => (
+          <div key={post.id}>{JSON.stringify(post)}</div>
+        ))}
       </div>
     </main>
   );
