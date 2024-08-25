6de7913e921b482920bf3c0f2e255f9cf360fcf0
MGSimard <g.marcgs@gmail.com>
2024-08-24 22:32:16 -0400

    Implemented role-based navigation rights for admin dashboard

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,4 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
+import { notFound } from "next/navigation";
+
 export default function Page() {
+  const currentUser = auth();
+
+  if (!currentUser.userId || currentUser.sessionClaims.metadata.role !== "admin") {
+    notFound();
+  }
+
   return (
     <main>
       <section>
