881dda715e52ca7f8c104ed134fcce039cc13a31
MGSimard <g.marcgs@gmail.com>
2024-08-29 10:11:29 -0400

    Add desc to enums

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -1,3 +1,8 @@
+// Game page sections, section enum db schema, validate section input in server action
 export const sectionEnums = ["resources", "communities", "creators"] as const;
+
+// Report form input choices, reportReason enum db schema, validate reportReason input in server action
 export const reportReasonEnums = ["reason1", "reason2", "reason3", "other"] as const;
+
+// reportStatus enum db schema (default to pending), validate moderation report status update in server action
 export const reportStatusEnums = ["pending", "approved", "denied"] as const;
