ac34ac61a836a9d64a2ee6959169d72d543231a2
MGSimard <g.marcgs@gmail.com>
2024-09-03 15:49:53 -0400

    Remove test buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,10 @@
-"use client";
 import { Searchbar } from "@/components/searchbar/Searchbar";
-import { toast } from "sonner";
-import { CustomToast } from "@/components/layout/CustomToast";
 
 export default function Page() {
   return (
     <main className="index-main">
       <Searchbar />
       <h2 className="notice">THIS APP IS IN ACTIVE DEVELOPMENT MODE.</h2>
-      <button
-        type="button"
-        onClick={() =>
-          toast.custom((t) => (
-            <CustomToast
-              icon="warning"
-              message="DATABASE ERROR: Could not retrieve current vote or submission score."
-            />
-          ))
-        }>
-        TEST ERROR
-      </button>
-      <button
-        type="button"
-        onClick={() =>
-          toast.custom((t) => <CustomToast icon="success" message="SUCCESS: Vote successfully modified." />)
-        }>
-        TEST SUCCESS
-      </button>
     </main>
   );
 }
