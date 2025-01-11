12de0bf9265bd02a760504ea7b43ac42afb3a5d6
MGSimard <g.marcgs@gmail.com>
2025-01-11 17:43:56 -0500

    I don't even use the middleware for protect I use manual RBAC

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/admin(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
