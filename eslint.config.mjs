3a9ff9e011bdbb7cb6e4624e64fb62f304a6664d
MGSimard <g.marcgs@gmail.com>
2025-01-14 03:01:45 -0500

    Also this

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
+/** @type {import("eslint").Linter.Config[]} */
 export default [
   ...compat.extends(
     "next/core-web-vitals",
