97df93c282b4fdff7e048a8b23ed7829446b1264
MGSimard <mgsimard.dev@gmail.com>
2025-02-07 19:26:07 -0500

    Fix system default theme on initial render

--- src/components/layout/ThemeToggle.tsx
+++ src/components/layout/ThemeToggle.tsx
@@ -1,18 +1,21 @@
 "use client";
-
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { PowerOn, PowerOff } from "@/components/icons";
 
 export function ThemeToggle() {
-  const theme = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, resolvedTheme } = useTheme();
 
   const themeToggle = () => {
-    theme.setTheme(theme.theme === "light" ? "dark" : "light");
+    setTheme(theme === "light" ? "dark" : "light");
   };
 
+  useEffect(() => setMounted(true), []);
+
   return (
-    <button type="button" onClick={themeToggle} className="themeToggle">
-      {theme.theme === "light" ? <PowerOn /> : <PowerOff />}
+    <button type="button" onClick={themeToggle} id="theme-toggle" title="Toggle theme" aria-label="Toggle theme">
+      {mounted && (resolvedTheme === "light" ? <PowerOn /> : <PowerOff />)}
     </button>
   );
 }
